675c76947a85e89624bfd0c5ffc96001ab2e812c
joelnyongesa <joelnyongesa148@gmail.com>
2023-09-08 20:59:55 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,65 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
 
 function App() {
   const [pets, setPets] = useState([]);
-  const [filters, setFilters] = useState({ type: "all" });
+  const [filters, setFilters] = useState("all");
+  const [url, setUrl] = useState(`http://localhost:3001/pets/`)
+  const [adopted, setAdopted] = useState(true)
+
+  function onChange(e){
+    setFilters(e.target.value)
+    setAdopted(!adopted)
+  }
+
+  function onFindPetsClick(){
+    if (filters === 'all'){
+      setUrl(`http://localhost:3001/pets/`)
+    }
+    else if (filters === "cat"){
+      setUrl(`http://localhost:3001/pets?type=cat`)
+    }
+    else if (filters === "dog"){
+      setUrl(`http://localhost:3001/pets?type=dog`)
+    }
+    else if(filters === "micropig"){
+      setUrl(`http://localhost:3001/pets?type=micropig`)
+    }
+  }
+
+  useEffect(()=>{
+    fetch(url)
+    .then(r => r.json())
+    .then(data => setPets(data))
+    .catch(e=> console.log(e))
+  }, [url, adopted])
+
+  // console.log(pets)
+
+  function onAdoptPet(pet){
+    fetch(url+pet.id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        id: pet.id,
+        type: pet.type,
+        gender: pet.gender,
+        age: pet.age,
+        weight: pet.weight,
+        name: pet.name,
+        isAdopted: true
+      })
+    })
+    console.log("adopted")
+    setAdopted(()=> !adopted)
+    // console.log(petId)
+  }
+
+  
 
   return (
     <div className="ui container">
@@ -15,10 +69,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChange={onChange} onFindPetsClick={onFindPetsClick}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={ pets } onAdoptPet={onAdoptPet}/>
           </div>
         </div>
       </div>
